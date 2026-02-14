ea5d90320554cd43537248c016f8518f027e42f8
Chris <elorduychristian@gmail.com>
2026-02-14 20:03:26 +0100

    Add exceptions, separate max.. from models to utils

---
+++ core/utils.py
@@ -0,0 +1,7 @@
+# Maximum allowed length for reminder text
+MAX_TEXT_LENGTH = 100
+
+MAX_EXPIRATION_DAYS = 7
+
+# Maximum expiration time allowed (7 days in minutes)
+MAX_EXPIRATION_MINUTES = 7 * 24 * 60
