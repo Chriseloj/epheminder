6a5c461900b96387b06c80a3a79a99c8566e3a08
Chris <elorduychristian@gmail.com>
2026-02-19 22:36:22 +0100

    Feat: add get_db_session

--- infrastructure/storage.py
+++ infrastructure/storage.py
@@ -1,6 +1,8 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import sessionmaker, declarative_base, Session
+from typing import Generator
+from contextlib import contextmanager
 
 STORAGE_DIR = "infrastructure/storage"
 os.makedirs(STORAGE_DIR, exist_ok=True)
@@ -20,3 +22,15 @@ except FileNotFoundError:
     pass
 
 Base.metadata.create_all(bind=engine)
+
+@contextmanager
+def get_db_session() -> Generator[Session, None, None]:
+    session = SessionLocal()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
